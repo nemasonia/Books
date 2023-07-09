7302ccc40af47227d8895799691399a5f15b9630
coo4 <coo4nemu5@gmail.com>
2023-07-09 17:19:04 +0900

    認証追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const { data: session } = useSession()
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          Hello&nbsp;
+          <code className='font-mono font-bold'>{session?.user?.name ?? 'guest'}</code>
         </p>
+        {!session && (
+          <button
+            onClick={() => signIn()}
+            className='flex w-full justify-center border-2 border-b border-red-300 hover:border-red-400 bg-red-200 hover:bg-red-300 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:hover:border-red-900 dark:bg-red-800/50 dark:hover:bg-red-900/30 lg:static lg:w-auto lg:rounded-xl lg:p-4'
+          >
+            Sign In
+          </button>
+        )}
+        {session && (
+          <button
+            onClick={() => signOut()}
+            className='flex w-full justify-center border-2 border-b border-green-300 bg-green-200 pb-6 pt-8 backdrop-blur-2xl dark:border-green-800 dark:bg-green-800/30 lg:static lg:w-auto lg:rounded-xl lg:p-4 hover:border-green-400 hover:bg-green-300 dark:hover:border-green-900 dark:hover:bg-green-900/30'
+          >
+            Sign Out
+          </button>
+        )}
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
